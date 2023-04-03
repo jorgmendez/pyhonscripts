ebc269c082fd469fbfebe16aec4bac872a1d6077
Jorge Mendez <123522646+jorgmendez@users.noreply.github.com>
2023-04-02 23:31:42 -0500

    Add files via upload

---
+++ send_sms.py
@@ -0,0 +1,23 @@
+
+from twilio.rest import Client
+
+
+account_sid = 'your_account_sid'
+auth_token = 'your_auth_token'
+
+
+to_number = '+1234567890' 
+from_number = '+0987654321' 
+
+
+client = Client(account_sid, auth_token)
+
+
+message = client.messages.create(
+    body='Hello, this is a test SMS from Twilio!', 
+    from_=from_number,
+    to=to_number
+)
+
+
+print('SMS sent! Message ID:', message.sid)
