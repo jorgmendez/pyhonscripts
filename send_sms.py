8773e0be4decf6e8d25c85d5dde15b62e989de83
Jorge Mendez <123522646+jorgmendez@users.noreply.github.com>
2023-04-02 23:32:17 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -5,7 +5,7 @@ from twilio.rest import Client
 account_sid = 'your_account_sid'
 auth_token = 'your_auth_token'
 
-
+# Define the phone number to send the SMS to and your Twilio phone number
 to_number = '+1234567890' 
 from_number = '+0987654321' 
 
