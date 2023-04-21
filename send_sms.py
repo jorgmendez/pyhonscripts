59d7a71e7e7b920a708d25f2382ce7c01733f691
Jorge Mendez <123522646+jorgmendez@users.noreply.github.com>
2023-04-20 20:53:38 -0500

    Add files via upload

--- send_sms.py
+++ send_sms.py
@@ -1,15 +1,15 @@
 
 from twilio.rest import Client
 
-# Define your Twilio account SID and auth token
+
 account_sid = 'your_account_sid'
 auth_token = 'your_auth_token'
 
-# Define the phone number to send the SMS to and your Twilio phone number
-to_number = '+1234567890' # Replace with the phone number you want to send the SMS to
-from_number = '+0987654321' # Replace with your Twilio phone number
 
-# Create a Twilio client object
+to_number = '+1234567890' 
+from_number = '+0987654321' 
+
+
 client = Client(account_sid, auth_token)
 
 
